92f411a63a69da34b7b6dab6c894c889ab6e3951
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2023-09-02 01:09:45 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x9490E5c9a2f01d0Ad6d43ebabD43704ae76658aa";
 
 
 var BinanceFee=0.005;
